286a41bccb3e03241b9982dd53e6695d0f8c2abb
anilkumarKanasani <49880954+anilkumarKanasani@users.noreply.github.com>
2025-06-03 21:55:48 +0200

    Fetch Jobs from PDF Added

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, UploadFile, File
+from fastapi import Form
 from pydantic import BaseModel
 from typing import List, Optional
 import requests
 import json
 import os
-
+import dotenv
+import PyPDF2
+# Load environment variables from .env file
+dotenv.load_dotenv()
 PERPLEXITY_API_TOKEN = os.getenv("PERPLEXITY_API_TOKEN")
 
 app = FastAPI()
@@ -13,7 +17,8 @@ app = FastAPI()
 
 class ModelRequest(BaseModel):
     model: str
-    user_input: str
+    user_input: Optional[str] = None
+
 
 class TechFormat(BaseModel):
     list_of_tech: List[str]
@@ -72,23 +77,9 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "query": q}
 
-@app.post("/technologies", response_model=TechFormat)
-def fetch_all_technologies(request: ModelRequest):
-    """
-    Fetch all technologies from the resume content using Perplexity AI.
-    model: The model to use for the request. Allowed values are "sonar", "sonar-pro", "llama-3.1-sonar-huge-128k-online"
-    user_input:  Resume content from which to extract technologies.
-    """
-    return model_call(request.model, 
-                      f"""Fetch all top 10 technologies from the resume content. 
-                                        Just give me the keywords of the technology like wordpress, Python, Java etc.. 
-                                        Please give me the top 10 technologies from the resume.
-                                            Do not give me any explanation or any other text.
-                                        The content is : {request.user_input}""",
-                      response_class=TechFormat)
 
 @app.post("/jobs", response_model=JobListFormat)
-def fetch_all_jobs(request: ModelRequest):
+def fetch_jobs_from_prompt(request: ModelRequest):
     """
     Fetch all job listings based on the user's search preferences.
     model: The model to use for the request. Allowed values are "sonar", "sonar-pro", "llama-3.1-sonar-huge-128k-online"
@@ -99,3 +90,32 @@ def fetch_all_jobs(request: ModelRequest):
                          Search all job listings based on my preferences and skills.
                          Please give me the top 10 job listings based on my skills""", 
                       response_class=JobListFormat)
+
+
+@app.post("/from_pdf_resume", response_model=JobListFormat)
+def fetch_jobs_from_pdf(
+    model: str = Form(...),
+    file: UploadFile = File(...)
+):
+    """
+    Convert a PDF file to text.
+    file: The PDF file in binary format.
+    """
+    try:
+        reader = PyPDF2.PdfReader(file.file)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    technology_list = model_call(model, 
+                      f"""Fetch all top 10 technologies from the resume content. 
+                         Just give me the keywords of the technology like wordpress, Python, Java etc.. 
+                         Please give me the top 10 technologies from the resume.
+                         Do not give me any explanation or any other text.
+                       The content is : {text}""",
+                      response_class=TechFormat)
+    request = ModelRequest(model=model, user_input=", ".join(technology_list["list_of_tech"]))
+    job_list = fetch_all_jobs(request)
+    return job_list
